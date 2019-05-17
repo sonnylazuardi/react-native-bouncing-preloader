cb1e5353b2ccbab575408f9d86e2e297dc222c0b
Kevin Hermawan <kevinhermawanx@gmail.com>
2019-05-17 13:03:48 +0700

    Add TypeScript declarations file

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+declare module 'react-native-bouncing-preloader' {
+  import React from 'react';
+
+  interface BouncingPreloaderProps {
+    icons: Array<string>;
+    leftRotation?: string;
+    rightRotation?: string;
+    leftDistance?: number;
+    rightDistance?: number;
+    speed?: number;
+    size?: number;
+  }
+
+  export default class BouncingPreloader extends React.Component<BouncingPreloaderProps> {}
+}
